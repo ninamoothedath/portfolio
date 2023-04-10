3e78b7d841ce0cac5c6be92d35111446ce2085b5
“ninina357” <ninawm@gmail.com>
2023-04-10 12:45:00 -0700

    design/content update

---
+++ script.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+	$("#writingt").click(function(){
+		$(".nav").fadeToggle()
+		$(".writing").fadeToggle()
+	});
+
+	$(".writing .return").click(function(){
+		$(".nav").fadeToggle()
+		$(".writing").fadeToggle()
+	});
+
+	$("#codet").click(function(){
+		$(".nav").fadeToggle()
+		$(".code").fadeToggle()
+	});
+
+	$(".code .return").click(function(){
+		$(".nav").fadeToggle()
+		$(".code").fadeToggle()
+	});
+
+	$("#motiont").click(function(){
+		$(".nav").fadeToggle()
+		$(".motion").fadeToggle()
+	});
+
+	$(".motion .return").click(function(){
+		$(".nav").fadeToggle()
+		$(".motion").fadeToggle()
+	});
+
+	$("#multit").click(function(){
+		$(".nav").fadeToggle()
+		$(".multi").fadeToggle()
+	});
+
+	$(".multi .return").click(function(){
+		$(".nav").fadeToggle()
+		$(".multi").fadeToggle()
+	});
+
+	$("#livet").click(function(){
+		$(".nav").fadeToggle()
+		$(".live").fadeToggle()
+		console.log("liveclick")
+	});
+
+	$(".live .return").click(function(){
+		$(".nav").fadeToggle()
+		$(".live").fadeToggle()
+	});
+
+	$("#datat").click(function(){
+		$(".nav").fadeToggle()
+		$(".data").fadeToggle()
+	});
+
+	$(".data .return").click(function(){
+		$(".nav").fadeToggle()
+		$(".data").fadeToggle()
+	});
+
+	
+});
