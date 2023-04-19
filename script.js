464fddfd9297b8f9208df82b2aa0a205a9ece10a
“ninina357” <ninawm@gmail.com>
2023-04-18 17:55:09 -0700

    data update

--- script.js
+++ script.js
@@ -66,5 +66,66 @@ $(document).ready(function(){
 		$("#frame").attr("src",vid)
 	})
 
+
+	// Data Graphic
+	$("#g1").hover(
+        function(){    
+        $("#g1").css("width","90%");
+        $("#g1").css("opacity","100%");
+        $("#g2, #g3, #g4").toggle("slow");
+        $(".caption").hide();
+        console.log("hovered!");
+      },function(){    
+        $("#g1").css("width","40%");
+        $("#g1").css("opacity","50%");
+        $("#g2, #g3, #g4").toggle("slow");
+        console.log("off!");
+        }
+      );
+
+      $("#g2").hover(
+        function(){    
+        $("#g2").css("width","90%");
+        $("#g2").css("opacity","100%");
+        $("#g1, #g3, #g4").toggle("slow");
+        $(".caption").hide();
+        console.log("hovered!");
+      },function(){    
+        $("#g2").css("width","40%");
+        $("#g2").css("opacity","50%");
+        $("#g1, #g3, #g4").toggle("slow");
+        console.log("off!");
+        }
+      );
+
+      $("#g3").hover(
+        function(){    
+        $("#g3").css("width","90%");
+        $("#g3").css("opacity","100%");
+        $("#g1, #g2, #g4").toggle("slow");
+        $(".caption").hide();
+        console.log("hovered!");
+      },function(){    
+        $("#g3").css("width","40%");
+        $("#g3").css("opacity","50%");
+        $("#g1, #g2, #g4").toggle("slow");
+        console.log("off!");
+        }
+      );
+
+      $("#g4").hover(
+        function(){    
+        $("#g4").css("width","90%");
+        $("#g4").css("opacity","100%");
+        $("#g1, #g2, #g3").toggle("slow");
+        $(".caption").hide();
+        console.log("hovered!");
+      },function(){    
+        $("#g4").css("width","40%");
+        $("#g4").css("opacity","50%");
+        $("#g1, #g2, #g3").toggle("slow");
+        console.log("off!");
+        }
+      );
 	
 });
